8b95bd419300b35fdf43497d46bdc058aca1da15
Станислав <@->
2017-08-30 08:04:18 +0300

    Add project files.

---
+++ Lesson7-Arrays_2908/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_Arrays_2908
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
